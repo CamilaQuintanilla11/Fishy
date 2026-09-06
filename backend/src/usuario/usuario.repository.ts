e643042255a253d5b3dd3d22bb722dea392b8678
CamilaQuintanilla11 <camilaquintanillacabrera@gmail.com>
2026-09-05 20:20:41 -0600

    Update usuario.repository.ts

--- backend/src/usuario/usuario.repository.ts
+++ backend/src/usuario/usuario.repository.ts
@@ -6,4 +6,21 @@ const COLUMNS = 'id, correo, nombre, contrasenaHash, rol_id, created_at';
 export class UsuarioRepository {
     constructor(@Inject(DB_POOL) private readonly pool: Pool) { }
 
+    async findAll(): Promise<Usuario[]>{
+        const [rows] = await this.pool.query<RowDataPacket[]>(
+            `SELECT ${COLUMNS} FROM usuario ORDER BY createdAt DESC`
+        );
+        return rows.map(toEntity)
+    }
+}
+
+function toEntity(row: any): Usuario {
+    const usuario = new Usuario();
+    usuario.id = row.id;
+    usuario.nombre = row.nombre;
+    usuario.correo = row.correo;
+    usuario.contrasenaHash = row.contrasenaHash;
+    usuario.tieneRol = row.tieneRol;
+    usuario.fecha_creado = row.fecha_creado;
+    return usuario;
 }
